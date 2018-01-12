0249c7cd8655128b1f5c1a2fd10034cc3b40b32e
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-12 16:49:30 +1100

    Add cors middleware

--- server.js
+++ server.js
@@ -3,12 +3,14 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const authMiddleware = require('./middleware/auth')
 
 const server = express()
 
 // Middleware Plugins
 server.use(bodyParser.json()) // Allows me to have JSON uploads (POST/PATCH/PUT)
+server.use(cors()) // Allow other origins to access, i.e our react front-end
 server.use(authMiddleware.initialize) // Kick passport off
 
 // Routes
@@ -31,3 +33,5 @@ server.listen(7000, error => {
   if (error) console.error('Error starting', error)
   else console.log('Started at http://localhost:7000')
 })
+
+module.exports = server
