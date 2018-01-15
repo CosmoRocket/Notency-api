0946f29bf05f79a2f44e607be05742dae7c3a04e
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-16 00:01:59 +1100

    Create send and groupSend routes

--- routes/sms.js
+++ routes/sms.js
@@ -5,10 +5,16 @@ const Recipient = require('../models/Recipient')
 const Message = require('../models/Message')
 const messageParser = require('../helper/message-parser')
 const smsMiddleware = require('../middleware/sms')
-// const authMiddleware = require('../middleware/auth')
+const authMiddleware = require('../middleware/auth')
 const router = express.Router()
 
 // PATCH - Add responses to notification
 router.patch('/sms/receive', smsMiddleware.receiveSms)
+
+// POST - Send a Text Message
+router.post('/sms/send', authMiddleware.requireJWT, smsMiddleware.sendSms)
+
+// POST - Send a Group Text Message
+router.post('/sms/groupSend', authMiddleware.requireJWT, smsMiddleware.sendGroupSms)
 
 module.exports = router
