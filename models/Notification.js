23b6357c395f18c7a255b3fbe504c15d880f3e7c
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-15 15:24:57 +1100

    Update notification model to include recipients

--- models/Notification.js
+++ models/Notification.js
@@ -9,12 +9,14 @@ Notification Model
 Code - Unique - e.g. EARTHQUAKE1, TSUNAMI1, FRANCETERROR
 Subject - Unique - e.g. Earthquake at Melbourne, Tsunami at Sydney Harbour, Terror Attack in France
 Body - Character Limit? - e.g. This is to inform all Students that there has been an Earthquake at Melbourne. Please reply "EARTHQUAKE1 OK" if you are safe.
-Responses = Array of Message [Recipient ID and Body]
+Recipients = Array of Recipient [Recipient]
+Responses = Array of Message [Sender (Recipient) and Body]
 */
 const notificationSchema = new Schema({
   code: { type: String, required: true, unique: true },
   subject: { type: String, required: true, unique: true },
   body: { type: String, required: true },
+  recipients: [{ type: Schema.ObjectId, ref: 'Recipient', default: [] }],
   responses: [{ type: Schema.ObjectId, ref: 'Message', default: [] }],
   createdAt: { type: Date, default: Date.now }
 })
