39de29c38c5688a9c5a51476c4d9db194d9b4f16
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-16 12:56:13 +1100

    Add bodyHtml and groups fields

--- models/Announcement.js
+++ models/Announcement.js
@@ -7,12 +7,14 @@ const Schema = mongoose.Schema
 /*
 Announcement Model
 Subject - Unique - e.g. Earthquake at Melbourne, Tsunami at Sydney Harbour, Terror Attack in France
-Body - Character Limit? - e.g. This is to inform all Students that there has been an Earthquake at Melbourne. Please reply "EARTHQUAKE1 OK" if you are safe.
+Body Html - Character Limit? - e.g. <html><body><p>This is to inform all Students that there has been an Earthquake at Melbourne. Please reply "EARTHQUAKE1 OK" if you are safe.</p></body></html>
+Groups - e.g. Australia, France
 Recipients = Array of Recipient [Recipient]
 */
 const announcementSchema = new Schema({
   subject: { type: String, required: true, unique: true },
-  body: { type: String, required: true },
+  bodyHtml: { type: String, required: true },
+  groups: { type: String, required: true },
   recipients: [{ type: Schema.ObjectId, ref: 'Recipient', default: [] }],
   createdAt: { type: Date, default: Date.now }
 })
