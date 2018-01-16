b46763e1a96e38c8a97b9afe373ae687e4ddece2
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-16 12:56:18 +1100

    Add bodyHtml and groups fields

--- models/Notification.js
+++ models/Notification.js
@@ -9,6 +9,8 @@ Notification Model
 Code - Unique - e.g. EARTHQUAKE1, TSUNAMI1, FRANCETERROR
 Subject - Unique - e.g. Earthquake at Melbourne, Tsunami at Sydney Harbour, Terror Attack in France
 Body - Character Limit? - e.g. This is to inform all Students that there has been an Earthquake at Melbourne. Please reply "EARTHQUAKE1 OK" if you are safe.
+Body Html - Character Limit? - e.g. <html><body><p>This is to inform all Students that there has been an Earthquake at Melbourne. Please reply "EARTHQUAKE1 OK" if you are safe.</p></body></html>
+Groups - e.g. Australia, France
 Recipients = Array of Recipient [Recipient]
 Responses = Array of Message [Sender (Recipient) and Body]
 */
@@ -16,6 +18,8 @@ const notificationSchema = new Schema({
   code: { type: String, required: true, unique: true },
   subject: { type: String, required: true, unique: true },
   body: { type: String, required: true },
+  bodyHtml: { type: String, required: true },
+  groups: { type: String, required: true },
   recipients: [{ type: Schema.ObjectId, ref: 'Recipient', default: [] }],
   responses: [{ type: Schema.ObjectId, ref: 'Message', default: [] }],
   createdAt: { type: Date, default: Date.now }
