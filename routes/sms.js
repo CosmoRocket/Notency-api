c5f12c0e9a27a69bd4bd42d78be81317a59850fd
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-16 14:58:16 +1100

    Remove model imports

--- routes/sms.js
+++ routes/sms.js
@@ -1,9 +1,5 @@
 /* @flow */
 const express = require('express')
-const Notification = require('../models/Notification')
-const Recipient = require('../models/Recipient')
-const Message = require('../models/Message')
-const messageParser = require('../helper/message-parser')
 const smsMiddleware = require('../middleware/sms')
 const authMiddleware = require('../middleware/auth')
 const router = express.Router()
