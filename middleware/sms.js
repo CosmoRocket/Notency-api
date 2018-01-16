21cff2a599f8b7999eb4c020685b68986e747b56
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-16 12:04:26 +1100

    Update error handling

--- middleware/sms.js
+++ middleware/sms.js
@@ -9,7 +9,7 @@ const twilio = require('./twilio')
 *  Receive SMS message
 */
 // $FlowFixMe - Turn off type annotations
-const receiveSms = async (req, res, next) => {
+const receiveSms = async (req, res) => {
   try {
     // Get the JSON
     const attributes = req.body
@@ -30,25 +30,26 @@ const receiveSms = async (req, res, next) => {
       ? messageParser.parseCodeFromMessage(body)
       : ''
     if (!code) throw new Error('Invalid response message')
+    else {
+      // Create the response as a Message object
+      const responseMessage = await Message.create(messageAttribute)
 
-    // Create the response as a Message object
-    const responseMessage = await Message.create(messageAttribute)
-
-    // Update the notification and append the response
-    const notification = await Notification.findOneAndUpdate(
-      { code: code },
-      { $addToSet: { responses: responseMessage } },
-      { upsert: true, new: true, runValidators: true }
-    ).populate({
-      path: 'responses',
-      populate: {
-        path: 'sender',
-        model: 'Recipient'
-      }
-    })
-    res.status(200).json(notification)
+      // Update the notification and append the response
+      const notification = await Notification.findOneAndUpdate(
+        { code: code },
+        { $addToSet: { responses: responseMessage } },
+        { upsert: true, new: true, runValidators: true }
+      ).populate({
+        path: 'responses',
+        populate: {
+          path: 'sender',
+          model: 'Recipient'
+        }
+      })
+      res.status(200).json(notification)
+    }
   } catch (error) {
-    next(error)
+    res.status(400).json(error.message)
   }
 }
 
@@ -56,14 +57,14 @@ const receiveSms = async (req, res, next) => {
 *  Send SMS message
 */
 // $FlowFixMe - Turn off type annotations
-const sendSms = async (req, res, next) => {
+const sendSms = async (req, res) => {
   try {
     const attributes = req.body
     const data = await twilio.sendSMS(attributes.recipient, attributes.message)
     res.status(201).json(data)
   }
-  catch(error) {
-    next(error)
+  catch (error) {
+    res.status(400).json(error.message)
   }
 }
 
@@ -71,14 +72,14 @@ const sendSms = async (req, res, next) => {
 *  Send SMS message
 */
 // $FlowFixMe - Turn off type annotations
-const sendGroupSms = async (req, res, next) => {
+const sendGroupSms = async (req, res) => {
   try {
     const attributes = req.body
     const data = await twilio.sendGroupSMS(attributes.recipients, attributes.message)
     res.status(201).json(data)
   }
-  catch(error) {
-    next(error)
+  catch (error) {
+    res.status(400).json(error.message)
   }
 }
 
@@ -87,72 +88,3 @@ module.exports = {
   sendSms,
   sendGroupSms
 }
-
-// const receiveSms = (req, res, next) => {
-//   // Get the JSON
-//   const attributes = req.body
-//   const mobile = attributes.From
-//   const body = attributes.Body
-
-//   // Get Sender as a Recipient using a Mobile number
-//   Recipient.findOne({ mobile: mobile })
-//     .then(sender => {
-//       // console.log('sender', sender)
-
-//       // Create a Message based on the JSON
-//       const messageAttribute = {
-//         sender: sender,
-//         body: body
-//       }
-//       let code = ''
-
-//       // Parse code from message body
-//       if( messageParser.isValidResponse(body) ) {
-//         code = messageParser.parseCodeFromMessage(body)
-//       }
-//       else {
-//         throw new Error('Invalid response message')
-//       }
-
-//       Message.create(messageAttribute)
-//         .then(responseMessage => {
-//           // console.log('responseMessage', responseMessage)
-//           // console.log('Update Notification')
-
-//           // Update the notification and append the response
-//           Notification.findOneAndUpdate(
-//             { code: code },
-//             { $addToSet: { responses: responseMessage } },
-//             { upsert: true, new: true, runValidators: true }
-//           )
-//             .populate({
-//               path: 'responses',
-//               populate: {
-//                 path: 'sender',
-//                 model: 'Recipient'
-//               }
-//             })
-//             .then(notification => {
-//               // console.log('notification', notification)
-//               // console.log('notification.responses[0].sender', notification.responses[0].sender)
-
-//               res.status(200).json(notification)
-//             })
-//             .catch(error => {
-//               // console.error(error)
-//               // res.status(400).json({ error })
-//               next(error)
-//             })
-//         })
-//         .catch(error => {
-//           // console.error(error)
-//           // res.status(400).json({ error })
-//           next(error)
-//         })
-//     })
-//     .catch(error => {
-//       // console.error(error)
-//       // res.status(400).json({ error })
-//       next(error)
-//     })
-// }
