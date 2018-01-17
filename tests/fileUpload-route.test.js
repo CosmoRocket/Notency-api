846fda90fc32297e95a82c68507534587cf3d41e
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-17 15:48:35 +1100

    Update tests for uploading empty forms

--- tests/fileUpload-route.test.js
+++ tests/fileUpload-route.test.js
@@ -18,21 +18,34 @@ const api = axios.create({
 //   return api.defaults.headers.common['Authorization']
 // }
 
-describe('Upload a file', () => {
-  test('It should upload the file', async () => {
+// describe('Upload a file', () => {
+//   test('It should upload the file', async () => {
+//     try {
+//       const formData = new FormData()
+//       formData.append('csvFile', FS.createReadStream('./tests/sample.csv'))
+//       const response = await api.post('/upload', formData, {
+//         headers: formData.getHeaders()
+//       })
+//       expect(response.status).toBe(201)
+//     }
+//     catch (error) {
+//       expect(error).toBeFalsy()
+//     }
+//   })
+// })
 
+describe('Upload an empty form', () => {
+  test('It should not upload a file', async () => {
     try {
-      // const fileData = await readFilePromise('./tests/sample.csv')
       const formData = new FormData()
-      formData.append('files.csvFile', FS.createReadStream('./tests/sample.csv'))
+      formData.append('csvFile', FS.createReadStream('./tests/sample.csv'))
       const response = await api.post('/upload', formData, {
         headers: formData.getHeaders()
       })
-      expect(response.status).toBe(201)
+      expect('It should catch an error').toBeNull()
     }
-    catch(error) {
-      expect(error).toBeFalsy()
+    catch (error) {
+      expect(error.response.status).toBe(400)
     }
-    
   })
 })
