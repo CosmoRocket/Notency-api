d42ac6bf73fff0e0516c3d1740c50c1634fcd10b
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-18 15:35:57 +1100

    Add new input fields for exceljs

--- middleware/exceljs.js
+++ middleware/exceljs.js
@@ -37,30 +37,37 @@ const readFromCsv = filename => {
     try {
       workbook.csv.readFile(filename).then(worksheet => {
         // Get the column names
-        const label1 = worksheet.getRow(1).values[1] // Student ID
+        const label1 = worksheet.getRow(1).values[1] // ID Number
         const label2 = worksheet.getRow(1).values[2] // First Name
         const label3 = worksheet.getRow(1).values[3] // Last Name
-        const label4 = worksheet.getRow(1).values[4] // Mobile
-        const label5 = worksheet.getRow(1).values[5] // Email
+        const label4 = worksheet.getRow(1).values[4] // Role
+        const label5 = worksheet.getRow(1).values[5] // Mobile
+        const label6 = worksheet.getRow(1).values[6] // Email
+        const label7 = worksheet.getRow(1).values[7] // Nationality
+        const label8 = worksheet.getRow(1).values[8] // Graduation Date
         
         // Iterate over each row
         worksheet.eachRow((row, rowNumber) => {
           // console.log('Row ' + rowNumber + ' = ' + JSON.stringify(row.values))
           if (rowNumber > 1) {
+            // console.log('row.values', row.values)
             // Read column values
             records.push({
-              [label1]: row.values[1], // Student ID
+              [label1]: row.values[1], // ID Number
               [label2]: row.values[2], // First Name
               [label3]: row.values[3], // Last Name
-              [label4]: row.values[4], // Mobile
-              [label5]: row.values[5] // Email
+              [label4]: row.values[4], // Role
+              [label5]: row.values[5], // Mobile
+              [label6]: row.values[6], // Email
+              [label7]: row.values[7], // Nationality
+              [label8]: row.values[8] // Graduation Date
             })
           }
         })
         success(records)
       })
     }
-    catch(error) {
+    catch (error) {
       fail(error)
     }
   })
