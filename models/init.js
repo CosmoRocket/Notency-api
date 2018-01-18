6feffa279f24edefcd92e384da35c946504eb7f8
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-19 01:27:52 +1100

    Add dotenv

--- models/init.js
+++ models/init.js
@@ -1,6 +1,7 @@
 /* @flow */
 const mongoose = require('mongoose')
-
+// Dotenv - Necessary to be placed here for process.env to be recognised on runtime
+require('dotenv').config()
 // Use the Promise functionality built into Node.js
 mongoose.Promise = global.Promise
 
