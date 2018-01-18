787234be77c87b139a8fd4a245d83358dcd1513d
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-19 00:52:29 +1100

    Update receive routes to POST

--- routes/sms.js
+++ routes/sms.js
@@ -4,8 +4,8 @@ const smsMiddleware = require('../middleware/sms')
 const authMiddleware = require('../middleware/auth')
 const router = express.Router()
 
-// PATCH - Add responses to notification
-router.patch('/sms/receive', smsMiddleware.receiveSms)
+// POST - Add responses to notification
+router.post('/sms/receive', smsMiddleware.receiveSms)
 
 // POST - Send a Text Message
 router.post('/sms/send', authMiddleware.requireJWT, smsMiddleware.sendSms)
