c49c73b1726d8bf5136e1f4152dac5c2400798c4
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-18 15:37:20 +1100

    Add storing of csv to Recipient model

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,6 +1,8 @@
 const multer = require('multer')
 const path = require('path')
+const Recipient = require('../models/Recipient')
 const { readFromCsv } = require('./exceljs')
+const { formatRecordsForRecipients } = require('../helper/recipient-parser')
 
 // Configure Storage
 const storage = multer.diskStorage({
@@ -19,18 +21,44 @@ const upload = multer({ storage })
 // Upload File Request/Response
 const uploadFile = async (req, res) => {
   try {
-    console.log('File Uploaded', req.file)
-    
     // Upload folder
     const UPLOAD_FOLDER = 'files'
     const FILE_NAME = req.file.originalname
-
     // Read the CSV File
-  
-    const records = await readFromCsv(UPLOAD_FOLDER+'/'+FILE_NAME)
-    res.status(201).json({ records })
+    const records = await readFromCsv(UPLOAD_FOLDER + '/' + FILE_NAME)
+    // Format records as Recipients
+    const recipientRecords = formatRecordsForRecipients(records)
+    // Array of ID Numbers to be activated in Records
+    // const activeIdNumbers = getIDNumbersFromRecords(records)
+
+    // console.log(recipientRecords)
+    if (recipientRecords) {
+
+      // Deactivate all Records first
+      await Recipient.update(
+        { },
+        { $set: { active: false } },
+        { multi: true, upsert: false, new: true, runValidators: true }
+      )
+
+      // Store records to Recipients and Activate
+      const results = await Promise.all(recipientRecords.map((recipientAttributes) => {
+        return Recipient.findOneAndUpdate(
+          { idNo: recipientAttributes.idNo },
+          { $set: recipientAttributes },
+          { upsert: true, new: true, runValidators: true }
+        )
+      }))
+
+      // console.log(deactivatedRecords)
+      res.status(201).json(results)
+    }
+    else {
+      res.status(400).json({ error: 'Invalid CSV File' })
+    }
   }
-  catch(error) {
+  catch (error) {
+    console.log(error)
     res.status(400).json({ error })
   }
 
