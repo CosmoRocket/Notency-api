eb29e847c1ed0b7b6204a4c1bb206720eebff10b
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-21 22:25:54 +1100

    Create pusher middleware

---
+++ middleware/pusher.js
@@ -0,0 +1,32 @@
+const Pusher = require('pusher')
+
+const pushMessage = (message, channel, event) => {
+  const PUSHER_APP_ID = process.env.PUSHER_APP_ID
+  const PUSHER_KEY = process.env.PUSHER_KEY
+  const PUSHER_SECRET = process.env.PUSHER_SECRET
+  const PUSHER_CLUSTER = process.env.PUSHER_CLUSTER
+
+  const pusher = new Pusher({
+    appId: PUSHER_APP_ID,
+    key: PUSHER_KEY,
+    secret: PUSHER_SECRET,
+    cluster: PUSHER_CLUSTER,
+    encrypted: true
+  })
+
+  return new Promise((success, fail) => {
+    if (!!message) {
+      pusher.trigger(channel, event, {
+        "message": message
+      })
+      success(message)
+    }
+    else {
+      fail('No message specified')
+    }
+  })
+}
+
+module.exports = {
+  pushMessage
+}
