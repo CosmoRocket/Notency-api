c818a154829c6559dbf00c4f569da3b7229b4c25
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-23 13:19:14 +1100

    Add file type validation for csv

--- middleware/multer.js
+++ middleware/multer.js
@@ -26,29 +26,36 @@ const uploadFile = async (req, res) => {
     // Upload folder
     const UPLOAD_FOLDER = '/tmp'
     const FILE_NAME = req.file ? req.file.originalname : ''
+    const FILE_TYPE = req.file.mimetype
     // If File Name is present
     if (FILE_NAME) {
-      // Read the CSV File
-      const records = await readFromCsv(UPLOAD_FOLDER + '/' + FILE_NAME)
-      // Format records as Recipients
-      const recipientRecords = formatRecordsForRecipients(records)
-      // Recipient Records are present
-      if (recipientRecords) {
-        // First, deactivate all Records in Recipients
-        await Recipient.update(
-          {},
-          { $set: { active: false } },
-          { multi: true, upsert: false, new: true, runValidators: true }
-        )
-        // Store records to Recipients and Activate
-        const results = await Promise.all(recipientRecords.map((recipientAttributes) => {
-          return Recipient.findOneAndUpdate(
-            { idNo: recipientAttributes.idNo },
-            { $set: recipientAttributes },
-            { upsert: true, new: true, runValidators: true }
+      // If File Type is CSV
+      if (FILE_TYPE === 'text/csv') {
+        // Read the CSV File
+        const records = await readFromCsv(UPLOAD_FOLDER + '/' + FILE_NAME)
+        // Format records as Recipients
+        const recipientRecords = formatRecordsForRecipients(records)
+        // Recipient Records are present
+        if (recipientRecords) {
+          // First, deactivate all Records in Recipients
+          await Recipient.update(
+            {},
+            { $set: { active: false } },
+            { multi: true, upsert: false, new: true, runValidators: true }
           )
-        }))
-        res.status(201).json(results)
+          // Store records to Recipients and Activate
+          const results = await Promise.all(recipientRecords.map((recipientAttributes) => {
+            return Recipient.findOneAndUpdate(
+              { idNo: recipientAttributes.idNo },
+              { $set: recipientAttributes },
+              { upsert: true, new: true, runValidators: true }
+            )
+          }))
+          res.status(201).json(results)
+        }
+        else {
+          res.status(400).json({ error: 'Invalid CSV File' })
+        }
       }
       else {
         res.status(400).json({ error: 'Invalid CSV File' })
