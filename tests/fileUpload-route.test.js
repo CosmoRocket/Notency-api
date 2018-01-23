df60e023f5a73bd473bc416b7cbfb06fa05d41d0
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-23 13:19:18 +1100

    Update file upload test

--- tests/fileUpload-route.test.js
+++ tests/fileUpload-route.test.js
@@ -1,7 +1,7 @@
 /* @flow */
-
 const axios = require('axios')
 const FormData = require('form-data')
+const User = require('../models/User')
 // const Util = require('util')
 const FS = require('fs')
 
@@ -11,28 +11,35 @@ const api = axios.create({
   baseURL: 'http://localhost:7001', // Test Server
 })
 
-// const setToken = token => {
-//   api.defaults.headers.common['Authorization'] = `Bearer ${token}`
-// }
-// const getToken = () => {
-//   return api.defaults.headers.common['Authorization']
-// }
+const setToken = token => {
+  api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
+const getToken = () => {
+  return api.defaults.headers.common['Authorization']
+}
 
-// describe('Upload a file', () => {
-//   test('It should upload the file', async () => {
-//     try {
-//       const formData = new FormData()
-//       formData.append('csvFile', FS.createReadStream('./tests/sample.csv'))
-//       const response = await api.post('/upload', formData, {
-//         headers: formData.getHeaders()
-//       })
-//       expect(response.status).toBe(201)
-//     }
-//     catch (error) {
-//       expect(error).toBeFalsy()
-//     }
-//   })
-// })
+const user = {
+  username: 'admin',
+  password: 'password123'
+}
+
+beforeAll(async () => {
+  try {
+    await User.deleteMany()
+    console.log('Test started. Data deleted.')
+    // Create a User
+    while(!getToken()) {
+      const response = await api.post('/auth/register', user)
+      const token = response.data.token
+      if (token) {
+        setToken(token)
+        console.log('Token created', token)
+      }
+    }
+  } catch (error) {
+    console.error('Error deleting data', error)
+  }
+})
 
 describe('Upload an empty form', () => {
   test('It should not upload a file', async () => {
@@ -48,4 +55,14 @@ describe('Upload an empty form', () => {
       expect(error.response.status).toBe(400)
     }
   })
+})
+
+afterAll(async () => {
+  // Delete all recipient data
+  try {
+    await User.deleteMany()
+    console.log('Test ended. Data deleted.')
+  } catch (error) {
+    console.error('Error deleting data', error)
+  }
 })
